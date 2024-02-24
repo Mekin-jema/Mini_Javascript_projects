55849f7379e87bdc835d1f41c8fc35ce04f174b3
Mekin Jemal <mekinjemal999@gmail.com>
2024-02-24 09:27:02 +0300

    the object  array is transformed into  product.js

--- Amazon_website/scripts/amazon.js
+++ Amazon_website/scripts/amazon.js
@@ -1,41 +1,6 @@
 
 //Here is the first step  (saving data)
-// the list and array of the following are sample of dataStructure
-        const products=[
-    {  //create object using { notation}
-        image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating:{
-            stars:4.5,
-            count:87
-        },
-        priceCount:1090// javasctipt has problem with decimal as a result $ should be 
-                    //converted to cent, to do this multiply by 100(1dollar=100cent)
-
-        },
-    {
-        image:"images/products/intermediate-composite-basketball.jpg",
-        name:'Intermediate Size Basketball',
-        rating:{
-            stars:4,
-            count:127
-        },
-        priceCount:2095
-        },
-
-
-    {
-        image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating:{
-            stars:4.5,
-            count:56
-
-        },
-        priceCount:799
-
-}
-];
+// the list and array of the following are sample of dataStructu
   let productsHtml='';
       const productsGrid=document.querySelector('.products-grid')
       products.forEach((product)=>{
@@ -60,7 +25,7 @@
 
       <div class="product-price">
       $${
-          product.priceCount/100
+          (product.priceCount/100).toFixed(2)
       }
       </div>
 
