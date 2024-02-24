738f6ca747863c864fb766c155b175769b636d9a
Mekin Jemal <mekinjemal999@gmail.com>
2024-02-24 06:46:53 +0300

    some errors are handled

--- Amazon_website/scripts/amazon.js
+++ Amazon_website/scripts/amazon.js
@@ -1,4 +1,4 @@
-console.log("hello")
+
 //Here is the first step  (saving data)
 // the list and array of the following are sample of dataStructure
         const products=[
@@ -36,63 +36,61 @@ console.log("hello")
 
 }
 ];
-let productsHtml;
-const productsGrid=document.querySelector('.products-grid')
-products.forEach((product)=>{
-productsHtml +=`<div class="product-container">
-<div class="product-image-container">
-  <img class="product-image" src=${product.image}>
-</div>
+  let productsHtml='';
+      const productsGrid=document.querySelector('.products-grid')
+      products.forEach((product)=>{
+      productsHtml +=`<div class="product-container">
+      <div class="product-image-container">
+        <img class="product-image" src=${product.image}>
+      </div>
 
-<div class="product-name limit-text-to-2-lines">
-${product.name}
-</div>
+      <div class="product-name limit-text-to-2-lines">
+      ${product.name}
+      </div>
 
-<div class="product-rating-container">
-  <img class="product-rating-stars" 
-   src='images/ratings/rating-${product.rating.stars*10}.png'>
-  <div class="product-rating-count link-primary">
-    ${
-        product.rating.count
+      <div class="product-rating-container">
+        <img class="product-rating-stars" 
+        src='images/ratings/rating-${product.rating.stars*10}.png'>
+        <div class="product-rating-count link-primary">
+          ${
+              product.rating.count
+          }
+        </div>
+      </div>
+
+      <div class="product-price">
+      $${
+          product.priceCount/100
+      }
+      </div>
+
+      <div class="product-quantity-container">
+        <select>
+          <option selected="" value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      <div class="product-spacer"></div>
+
+      <div class="added-to-cart">
+        <img src="images/icons/checkmark.png">
+        Added
+      </div>
+
+      <button class="add-to-cart-button button-primary">
+        Add to Cart
+      </button>
+      </div>`
     }
-  </div>
-</div>
-
-<div class="product-price">
-$${
-    product.priceCount/100
-}
-</div>
-
-<div class="product-quantity-container">
-  <select>
-    <option selected="" value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-  </select>
-</div>
-
-<div class="product-spacer"></div>
-
-<div class="added-to-cart">
-  <img src="images/icons/checkmark.png">
-  Added
-</div>
-
-<button class="add-to-cart-button button-primary">
-  Add to Cart
-</button>
-</div>`
-productsGrid.innerHTML= productsHtml
-console.log( productsHtml)
-
-
-}
-)
+    )
+    // console.log( productsHtml)
+    productsGrid.innerHTML= productsHtml
